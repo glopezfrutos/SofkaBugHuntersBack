7533b655b6cedc8dd527fbb9c50302f32a5985a9
jpabloallin <jpac.647@gmail.com>
2022-06-24 22:04:33 -0500

    Created getProjectsUseCase and Get projects route.

--- src/main/java/com/sofka/bugsmanagement/routes/ProjectRoutes.java
+++ src/main/java/com/sofka/bugsmanagement/routes/ProjectRoutes.java
@@ -2,15 +2,16 @@ package com.sofka.bugsmanagement.routes;
 
 import com.sofka.bugsmanagement.model.ProjectDTO;
 import com.sofka.bugsmanagement.usecases.project.CreateProjectUseCase;
+import com.sofka.bugsmanagement.usecases.project.GetProjectsUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -25,6 +26,17 @@ public class ProjectRoutes {
                         .flatMap(projectDTO -> ServerResponse.status(HttpStatus.CREATED)
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .bodyValue(projectDTO))
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getProjectsRouter(GetProjectsUseCase getProjectsUseCase){
+        return route(
+                GET("/get/projects").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse
+                        .status(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getProjectsUseCase.get(), ProjectDTO.class))
         );
     }
 }
---
+++ src/main/java/com/sofka/bugsmanagement/usecases/project/GetProjectsUseCase.java
@@ -0,0 +1,27 @@
+package com.sofka.bugsmanagement.usecases.project;
+
+import com.sofka.bugsmanagement.Mappers.ProjectMapper;
+import com.sofka.bugsmanagement.model.ProjectDTO;
+import com.sofka.bugsmanagement.repositories.IProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Supplier;
+@Service
+@RequiredArgsConstructor
+public class GetProjectsUseCase implements Supplier<Flux<ProjectDTO>> {
+
+    private final IProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+
+    @Override
+    public Flux<ProjectDTO> get() {
+        return projectRepository
+                .findAll()
+                .map(project -> projectMapper
+                        .convertEntityToDto()
+                        .apply(project));
+    }
+}
