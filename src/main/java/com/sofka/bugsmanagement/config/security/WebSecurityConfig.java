49cd2058906a41da232de1f994d53f09be5b80e4
luis <luisfelorivas@gmai.com>
2022-06-27 22:37:18 -0500

    ADD Role authorization: the securityWebFilterChain was config in WebSecurityConfig to role  authentication. MyUserDetails and UserDetailsServiceImpl were added and User, UserDto and IUser  were modified in order to allow the role authorization.

--- src/main/java/com/sofka/bugsmanagement/config/security/WebSecurityConfig.java
+++ src/main/java/com/sofka/bugsmanagement/config/security/WebSecurityConfig.java
@@ -7,19 +7,39 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.CorsRegistration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
 @EnableWebFluxSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebFluxConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("**")
+                .allowedMethods("GET","PUT", "DELETE")
+                .allowedHeaders("**")
+                .exposedHeaders("**")
+                .allowCredentials(true).maxAge(3600);
+
+        // Add more mappings...
+    }
+
+
+
 
     @Bean
     SecurityWebFilterChain springWebFilter(ServerHttpSecurity http) {
 
         http.oauth2ResourceServer()
                 .jwt();
-        return http.csrf().disable()
+        return http.cors().disable()
+                .csrf().disable()
                 .authorizeExchange()
-                .pathMatchers(HttpMethod.GET,"/api/v1/project").permitAll()
+                .pathMatchers(HttpMethod.GET,"/api/v1/project").authenticated()
                 .pathMatchers(HttpMethod.GET,"/api/v1/bug").authenticated()
                 .pathMatchers(HttpMethod.GET,"/api/v1/task").hasAnyAuthority("READER","TESTER","ADMIN")
                 .pathMatchers("/api/v1/project").hasAnyAuthority("TESTER","ADMIN")
