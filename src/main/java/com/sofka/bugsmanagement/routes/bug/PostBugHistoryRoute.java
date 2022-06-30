c7f56b368a17ec1d3b80e69c061c39b84782637c
Esteban Carvajal <johan.gal.car@gmail.com>
2022-06-30 14:33:15 -0500

    pulling changes

--- src/main/java/com/sofka/bugsmanagement/routes/bug/PostBugHistoryRoute.java
+++ src/main/java/com/sofka/bugsmanagement/routes/bug/PostBugHistoryRoute.java
@@ -1,11 +1,8 @@
 package com.sofka.bugsmanagement.routes.bug;
 
-import com.sofka.bugsmanagement.model.bug.BugDto;
 import com.sofka.bugsmanagement.model.bug.BugHistoryDto;
 import com.sofka.bugsmanagement.usecases.bug.CreateBugHistoryUseCase;
-import com.sofka.bugsmanagement.usecases.bug.CreateBugUseCase;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -14,7 +11,6 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
 
 public class PostBugHistoryRoute {
 
