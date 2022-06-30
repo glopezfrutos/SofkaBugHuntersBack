72d08003275039c3d3a8676b7a8eaccfe958f7d5
luis <luisfelorivas@gmai.com>
2022-06-30 10:56:29 -0500

    Update use cases for create bug and task to added them to the history of each one.

--- src/main/java/com/sofka/bugsmanagement/usecases/bug/CreateBugUseCase.java
+++ src/main/java/com/sofka/bugsmanagement/usecases/bug/CreateBugUseCase.java
@@ -1,8 +1,10 @@
 package com.sofka.bugsmanagement.usecases.bug;
 
+import com.sofka.bugsmanagement.mappers.BugHistoryMapper;
 import com.sofka.bugsmanagement.mappers.BugMapper;
 import com.sofka.bugsmanagement.model.bug.BugDto;
 import com.sofka.bugsmanagement.repositories.IBugRepository;
+import com.sofka.bugsmanagement.repositories.history.IBugHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,12 +21,16 @@ public class CreateBugUseCase {
     private final IBugRepository repository;
     private final BugMapper mapper;
 
+    private final IBugHistoryRepository bugHistoryRepository;
+    private final BugHistoryMapper bugHistoryMapper;
+
     public Mono<BugDto> apply(@Valid BugDto bugDto) {
         log.info("\n***** New Bug created Id: {} *****\n", bugDto.getId());
-        return repository
+        return  bugHistoryRepository.save(bugHistoryMapper.convertBugDtoToBugHistory().apply(bugDto)).then(
+                repository
                 .save(mapper.convertDtoToEntity().apply(bugDto))
                 .map(dto -> mapper
                         .convertEntityToDto()
-                        .apply(dto));
+                        .apply(dto)));
     }
 }
