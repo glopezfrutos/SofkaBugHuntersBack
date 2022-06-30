272fe49761e0c817bb6fbc2d54ff78c623d1b720
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-06-30 18:04:15 -0300

    UpdateUserRoleUseCase refactored

--- src/main/java/com/sofka/bugsmanagement/usecases/user/UpdateUserRoleUseCase.java
+++ src/main/java/com/sofka/bugsmanagement/usecases/user/UpdateUserRoleUseCase.java
@@ -1,12 +1,15 @@
 package com.sofka.bugsmanagement.usecases.user;
 
 import com.sofka.bugsmanagement.mappers.UserMapper;
+import com.sofka.bugsmanagement.model.user.Role;
 import com.sofka.bugsmanagement.model.user.UserDto;
 import com.sofka.bugsmanagement.repositories.IUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -17,9 +20,17 @@ public class UpdateUserRoleUseCase {
     private final UserMapper mapper;
 
     public Mono<UserDto> apply(UserDto userDto) {
-        log.info("\n***** Role updated to {} for User {}  *****\n", userDto.getRole(), userDto.getId());
         return repository
-                .save(mapper.convertDtoToEntity().apply(userDto))
-                .map(entity -> mapper.convertEntityToDto().apply(entity));
+                .findAll()
+                .filter(entity -> Objects.equals(entity.getEmail(), userDto.getEmail()))
+                .collectList()
+                .flatMap(list -> {
+                    userDto.setId(list.get(0).getId());
+                    userDto.setSessionId(list.get(0).getSessionId());
+                    log.info("\n***** Role updated to {} for User {}  *****\n", userDto.getRole(), userDto.getEmail());
+                    return repository
+                            .save(mapper.convertDtoToEntity().apply(userDto))
+                            .map(entity -> mapper.convertEntityToDto().apply(entity));
+                });
     }
 }
